4860824568c334ce28136ecf0bbe1528dbdcea28
Nam Nguyen <namnguyenly2k1@gmail.com>
2022-01-16 09:59:19 +0700

    rain

---
+++ rain/scripts/index.js
@@ -0,0 +1,53 @@
+const body = document.querySelector("body")
+const amount = 50
+const duration = []
+
+let mouseX = -1
+let mouseY = -1
+
+function durationSetup() {
+  let i = 0
+  while (i < amount) {
+    duration.push(Math.random() * 5 + 1)
+    i++
+  }
+}
+
+function rain() {
+  let i = 0
+  while (i < amount) {
+    const drop = document.createElement("i")
+    durationSetup()
+
+    const size = 0.5 + Math.random() * 5
+    const posX = Math.floor(Math.random() * window.innerWidth)
+    const delay = Math.random() * -20
+
+    drop.style.width = size + "px"
+    drop.style.left = posX + "px"
+    drop.style.animationDelay = delay + "s"
+    drop.style.animationDuration = duration[i] + "s"
+
+    body.appendChild(drop)
+    i++
+  }
+}
+
+document.onmousemove = (event) => {
+  const dropList = document.querySelectorAll("i")
+  mouseX = event.pageX
+  mouseY = event.pageY
+  let i = 0
+  while (i < amount) {
+    const dropLeft = dropList[i].offsetLeft
+    if (dropLeft >= mouseX - 100 && dropLeft <= mouseX + 100) {
+      const newDuration = 40
+      dropList[i].style.animationDuration = newDuration + "s"
+    } else if (dropList[i].style.animationDuration === "40s") {
+      dropList[i].style.animationDuration = duration[i] + "s"
+    }
+    i++
+  }
+}
+
+rain()
