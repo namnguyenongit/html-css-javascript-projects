b2823a2b20969efb6d02a474bf63dad52faf4e0a
Nam Nguyen <namnguyenly2k1@gmail.com>
2022-02-17 20:27:00 +0700

    drag and drop

---
+++ drag and drop/main.js
@@ -0,0 +1,48 @@
+const item = document.querySelector('#item')
+const containers = document.querySelectorAll('.container')
+
+const dragStart = (e) => {
+  e.dataTransfer.setData('text/plain', e.target.id)
+  e.dataTransfer.dropEffect = 'move'
+  setTimeout(() => {
+    e.target.classList.add('hide')
+  })
+}
+
+const dragEnter = (e) => {
+  e.preventDefault()
+  e.target.classList.add('drag-over')
+}
+
+const dragOver = (e) => {
+  e.preventDefault()
+  e.target.classList.add('drag-over')
+  e.dataTransfer.dropEffect = 'move'
+}
+
+const dragLeave = (e) => {
+  e.target.classList.remove('drag-over')
+}
+
+const drop = (e) => {
+  e.target.classList.remove('drag-over')
+  const itemID = e.dataTransfer.getData('text/plain')
+  const newItem = document.getElementById(itemID)
+  e.target.appendChild(newItem)
+  newItem.classList.remove('hide')
+}
+
+const dragEnd = (e) => {
+  e.preventDefault()
+  item.classList.remove('hide')
+}
+
+item.addEventListener('dragstart', dragStart)
+
+containers.forEach((container) => {
+  container.addEventListener('dragenter', dragEnter)
+  container.addEventListener('dragover', dragOver)
+  container.addEventListener('dragleave', dragLeave)
+  container.addEventListener('dragend', dragEnd)
+  container.addEventListener('drop', drop)
+})
